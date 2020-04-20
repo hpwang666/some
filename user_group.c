34ff835ead71890cc59a7447a81fa8b11e9585d5
hpwang <1079339766@qq.com>
2020-04-20 08:10:05 +0800

    modify some

--- user_group.c
+++ user_group.c
@@ -6,7 +6,7 @@ groups work  查看work属于哪些组
 将一个已有用户 cnzhx 增加到一个已有用户组 apache 中，使此用户组成为该用户的附加用户组，
 可以使用带 -a 参数的 usermod  指令。-a 代表 append， 也就是将用户添加到新用户组中而不必离开原有的其他用户组。
 不过需要与 -G 选项配合使用：
-usermod -a cnzhx -G apache 
+usermod -a  -G apache cnzhx
 
 如果要同时将 cnzhx 的primary用户组改为 apache，则直接使用 -g 选项：
 usermod -g apache cnzhx
